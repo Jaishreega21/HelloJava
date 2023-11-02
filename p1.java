ce244624af04a97608f4aeb0e39430f7fc38aab5
Jaishreega21 <jaishreega21@gmail.com>
2023-11-02 14:15:37 +0530

    Adding java program

---
+++ p1.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+        System.out.println("This java file is created to deploy on tomcat");
+        System.out.println("Check the building phase");
+    }
+}
